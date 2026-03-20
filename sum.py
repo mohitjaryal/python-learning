db99f69019cbe2c32d5f24215adf8afbef4057b2
Mohit <mohitt.tech@gmail.com>
2026-03-20 23:12:49 +0530

    Update sum.py

--- sum.py
+++ sum.py
@@ -3,3 +3,4 @@
 num1 = int(input('Enter number 1 :'))
 num2 = int(input('Enter number 2 :'))
 
+operation = input('Which operation you want to perform : (1. Add \n 2. Subtract \n 3. Multiply \n 4. Divide\n)')

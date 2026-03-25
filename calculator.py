33c47a1b5ce718bb75b4df1dba227bae571f44b4
Mohit <mohitt.tech@gmail.com>
2026-03-25 22:22:40 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,3 +3,6 @@
 num = int(('Enter a number :'))
 
 # which operation user wants to perform
+operation = int(input('Enter which operation you want to perform (1. \n+ 2. \n- 3. * \n4. /)'))
+
+# main logic

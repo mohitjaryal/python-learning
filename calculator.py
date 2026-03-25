e0934a40d67230369d7a0c346a2d0e971bbfe67c
Mohit <mohitt.tech@gmail.com>
2026-03-25 22:29:54 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,8 +1,19 @@
 # basic calcuator using python
 
-num = int(('Enter a number :'))
+num1 = int(input('Enter number 1:'))
+num2 = int(input('Enter number 2:'))
 
 # which operation user wants to perform
-operation = int(input('Enter which operation you want to perform (1. \n+ 2. \n- 3. * \n4. /)'))
+operation = int(input('Enter which operation you want to perform \n1. + \n2. - \n3. * \n4. / \n :'))
 
 # main logic
+if operation == 1:
+    print('Sum :', num1 + num2)
+elif operation == 2:
+    print('Subtraction :', num1-num2)
+elif operation == 3:
+    print('Multiplication :', num1 * num2)
+elif operation == 4:
+    print('Division :', num1 / num2)
+else:
+    print('Error occured ! \n Please try again')

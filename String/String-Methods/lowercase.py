68025afaf625c8c8a6bac555f50e972a88fc6f47
Mohit <mohitt.tech@gmail.com>
2026-03-26 06:41:17 +0530

    string methods - upper,lower

---
+++ String/String-Methods/lowercase.py
@@ -0,0 +1,10 @@
+# converting string to lowercase
+
+# strings are immutable
+
+a = 'MOHIT JARYAL'
+
+# converting to uppercase
+
+# jb hm string m koi method lagate h to vo uski copy bna k changes krta h (original string as it is he rehti h q ki vo immutable hoti h )
+print(a.lower())
---
+++ String/String-Methods/uppercase.py
@@ -0,0 +1,10 @@
+# converting string to uppercase
+
+# strings are immutable
+
+a = 'mohit jaryal'
+print('Before :', a)
+# converting to uppercase
+
+# jb hm string m koi method lagate h to vo uski copy bna k changes krta h (original string as it is he rehti h q ki vo immutable hoti h )
+print(a.upper())

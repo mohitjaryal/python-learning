f0ab941c033092043f6689e89a25a1c8aadee5a5
Mohit <mohitt.tech@gmail.com>
2026-03-26 22:46:57 +0530

    program to greet the user according to current time

--- goodMorning.py
+++ goodMorning.py
@@ -2,7 +2,18 @@
 
 import time
 
-timestamp = time.strftime('%H:%M:%S')
-print(timestamp)
-timestamp = time.strftime('%H')
-print(timestamp)
+# taking user's name
+name = input('Enter your name :')
+
+# hours
+hour = int(time.strftime('%H'))
+print('Current time :',hour)
+
+if 5<= hour < 12:
+    print('Good morning Mr ',name)
+elif 12<= hour < 17:
+    print('Good Afternoon Mr ',name)
+elif 17 <= hour < 21:
+    print('Good Evening Mr ',name)
+else:
+    print('Good Night Mr ',name)

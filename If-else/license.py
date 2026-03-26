a8fc91cb8129d44f025fe2b3876ad0c52308b0f3
Mohit <mohitt.tech@gmail.com>
2026-03-26 22:23:17 +0530

    Update license.py

--- If-else/license.py
+++ If-else/license.py
@@ -1,1 +1,8 @@
 # WAP to check whether a person is elegible for license or not 
+
+age = int(input('Enter your age :'))
+
+if age >= 18:
+    print('Congrats you are elegible for Driving license !!')
+elif age < 18:
+    print

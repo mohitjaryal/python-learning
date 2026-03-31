6b3bc936e1106999840de9888d68761b04625300
Mohit <mohitt.tech@gmail.com>
2026-03-31 21:00:57 +0530

    Create list.py

---
+++ lists/list.py
@@ -0,0 +1,14 @@
+# basic demonstration of list
+
+li = [1,2,3,4,5,6,7,8,9,0]
+print(li)
+# printing the data type
+print(type(li))
+
+li2 = ['Mohit', 'Rohit', 'Ram', 'Shayam']
+print(li2)
+# printing the data type
+print(type(li2))
+
+# printng with the help of indexing
+print(li2[2])

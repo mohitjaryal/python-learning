b4860ebc369ff7138b41c2e68ebf0a2aade59dae
Mohit <mohitt.tech@gmail.com>
2026-04-01 23:26:13 +0530

    Create checkElement.py

---
+++ lists/basics/checkElement.py
@@ -0,0 +1,10 @@
+# check whether the element is present in the list or not 
+
+li = [1,2,3,4,564,57,8,3325,645]
+
+check = int(input('Btao konsa number dhundna hai :'))
+
+if check in li:
+    print('Ha bhai mil gya')
+else:
+    print('Nhi mila bhai sorry')

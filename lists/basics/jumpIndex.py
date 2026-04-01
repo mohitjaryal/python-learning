fd7cff06f7def0876dac869fad540146139b926a
Mohit <mohitt.tech@gmail.com>
2026-04-01 23:26:11 +0530

    Create jumpIndex.py

---
+++ lists/basics/jumpIndex.py
@@ -0,0 +1,10 @@
+# basic demonstration of jump index
+
+li = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+# before jump statement
+print(li)
+
+# after adding jump statement
+# isme 0th index se shuru hoga (mtlv 1 se) then 20th position tk chlega and hr 5th element ko print krega 
+print(li[0:20:5])

317f62668515dd7823e0bc7265b28210158d7a92
Mohit <mohitt.tech@gmail.com>
2026-04-02 23:44:53 +0530

    append

---
+++ lists/list-methods/append.py
@@ -0,0 +1,9 @@
+# .apend() method in list -> used to add new elements to the list
+
+li = [1,2,3,4,5]
+
+print('List before :',li)
+
+# adding 6 to list
+li.append(6)
+print('After adding new element :',li)

100c89f02571e162899036bd370535e346ffab8f
Mohit <mohitt.tech@gmail.com>
2026-04-03 12:14:41 +0530

    lists mehtods added

---
+++ lists/list-methods/concatenation.py
@@ -0,0 +1,7 @@
+# Concatenation (addition or merging) of lists
+
+lst1 = [1,2,3]
+
+lst2 = [4,5,6]
+
+print('Concatenation :', lst1 + lst2)
---
+++ lists/list-methods/copy.py
@@ -0,0 +1,9 @@
+# copy() method is used to create a shallow copy of a list.
+
+color  = ['red','green','blue','grey']
+
+new_list = color.copy()
+
+print('Original :', color)
+
+print('Copy :', new_list)

c5712a2be659335bbeecea022c3d29901653781b
Mohit <mohitt.tech@gmail.com>
2026-04-03 23:42:54 +0530

    final

--- list-methods/copy.py
+++
@@ -1,9 +0,0 @@
-# copy() method is used to create a shallow copy of a list.
-
-color  = ['red','green','blue','grey']
-
-new_list = color.copy()
-
-print('Original :', color)
-
-print('Copy :', new_list)
--- list-methods/extend.py
+++
@@ -1,9 +0,0 @@
-# .extend() method in a list is used to add multiple elements (from another iterable) to the end of a
-
-colors = ['voilet','indigo','blue']
-
-rainbow = ['green', 'yellow', 'orange', 'red']
-
-colors.extend(rainbow)
-
-print(colors)

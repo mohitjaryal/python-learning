26bc501af0ef12ad1177825e1b3796b2229d7090
Mohit <mohitt.tech@gmail.com>
2026-04-03 23:42:19 +0530

    Delete index.py

--- lists/list-methods/index.py
+++
@@ -1,9 +0,0 @@
-# .index() -> this returns the index of first occurance of the element in the list
-
-li = [1,5,4,4,5,6,5,7,4]
-
-# index of 5
-print('Index of 5 :',li.index(5))
-
-# index of 4
-print('Index of 4 :',li.index(4))

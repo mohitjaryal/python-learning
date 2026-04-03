9d4fffd6cebab2942c67a9fd005c6a86a824250c
Mohit <mohitt.tech@gmail.com>
2026-04-03 23:42:04 +0530

    Create insert.py

---
+++ list-methods/insert.py
@@ -0,0 +1,10 @@
+# insert() method in a list is used to add an element at a specific position (index).
+
+li = [ 1,2,3,4,6,7] 
+
+print('Before inserting :',li)
+
+# after inseting
+# syntax -> .insert(index, element)
+li.insert(4, 5)
+print('After inserting :', li)

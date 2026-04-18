d1cdaef50a85387ac021bfad2359b314f34af50c
Mohit <mohitt.tech@gmail.com>
2026-04-19 01:37:01 +0530

    lambda function basic demonstration

---
+++ functions/lambdaFunction.py
@@ -0,0 +1,11 @@
+# lambda function in python
+# it is a small anonymous function
+# it can take any number of arguments, but can only have one expression
+
+# Basic demonstration
+# write a lambda function to print the square of a number
+
+a = lambda x:x**2
+
+# function call
+print(a(2))

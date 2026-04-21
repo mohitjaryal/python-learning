6ef24936a460bc06c31598433355f56a3f2e31c0
Mohit <mohitt.tech@gmail.com>
2026-04-21 21:22:37 +0530

    Created add,subtract,multiply and division using magic methods

--- Creating-own-DataType/fractionDType.py
+++ Creating-own-DataType/fractionDType.py
@@ -3,11 +3,44 @@
 # creating class
 class Fraction:
 
-    # parameterized constructor -> means this is a constructor which needs an input 
+    # parameterized constructor
     def __init__(self,x,y):
         self.num = x
         self.den = y
 
-    # magic mehtod
+    # magic method
     def __str__(self):
         return '{} / {}'.format(self.num,self.den)
+    
+    # magic method
+    def __add__(self, other):
+        new_num = self.num*other.den + other.num*self.den
+        new_den = self.den*other.den
+        return Fraction(new_num,new_den)  
+    
+    # magic method
+    def __sub__(self, other):
+        new_num = self.num*other.den - other.num*self.den
+        new_den = self.den*other.den
+        return Fraction(new_num,new_den)
+    
+    # magic method
+    def __mul__(self, other):
+        new_num = self.num * other.num
+        new_den = self.den*other.den
+        return Fraction(new_num,new_den)
+    
+    # magic method
+    def __truediv__(self, other):
+        new_num = self.num * other.den
+        new_den = self.den*other.num
+        return Fraction(new_num,new_den)
+
+# creating object
+f1 = Fraction(3,4)
+f2 = Fraction(1,2)
+
+print(f1 + f2) 
+print(f1 - f2) 
+print(f1 * f2) 
+print(f1 / f2) 
